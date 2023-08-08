3c8a9c202fc3da8b05931d88650f26ae92db13a4
vasanc <vasanc2003@gmail.com>
2023-08-08 12:22:24 +0530

    projct setup+Side bar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider} from "@mui/material"
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard/Dashboard"
+import { Route, Routes } from "react-router-dom";
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value = {colorMode}>
+    <ThemeProvider theme = {theme}>
+      <CssBaseline/>
+      <div className="app">
+        <Sidebar/>
+        <main className="content">
+          <Topbar/>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}>
+            </Route>
+          </Routes>
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>)
+}
+
+export default App
